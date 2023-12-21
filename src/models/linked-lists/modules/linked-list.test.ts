99b43fdda02e983a768d7136ac07aac89a08b58e
Leonardo Rodrigues <leonardoacrg.dev@gmail.com>
2023-12-20 22:01:18 -0300

    test: add linked list unit tests

--- src/models/linked-lists/modules/linked-list.test.ts
+++ src/models/linked-lists/modules/linked-list.test.ts
@@ -27,11 +27,48 @@ describe('LinkedList', () => {
   });
 
   test('Should remove element at given position', () => {
-    console.log('Should remove element at given position');
     linkedList.push(firstElement);
     linkedList.push(secondElement);
     linkedList.insert(thirdElement, 0);
     expect(linkedList.removeAt(0)).toBe(thirdElement);
+  });
+
+  test('Should remove element', () => {
+    linkedList.push(firstElement);
+    linkedList.push(secondElement);
+    linkedList.insert(thirdElement, 0);
+    linkedList.remove(thirdElement);
+    expect(linkedList.head?.next?.element).toBe(secondElement);
+  });
+
+  test('Should get the index of a element', () => {
+    linkedList.push(firstElement);
+    linkedList.push(secondElement);
+    linkedList.insert(thirdElement, 0);
+    expect(linkedList.indexOf(thirdElement)).toBe(0);
+    expect(linkedList.indexOf(firstElement)).toBe(1);
+    expect(linkedList.indexOf(secondElement)).toBe(2);
+  });
+
+  test('Should get the size of the linked list', () => {
+    expect(linkedList.size()).toBe(0);
+    linkedList.push(firstElement);
+    expect(linkedList.size()).toBe(1);
+    linkedList.push(secondElement);
+    expect(linkedList.size()).toBe(2);
+    linkedList.insert(thirdElement, 0);
+    expect(linkedList.size()).toBe(3);
+    linkedList.remove(thirdElement);
+    expect(linkedList.size()).toBe(2);
+  });
+
+  test('Should get elements as string', () => {
+    linkedList.push(firstElement);
+    linkedList.push(secondElement);
+    linkedList.insert(thirdElement, 0);
+    expect(linkedList.toString()).toBe(
+      `${thirdElement}, ${firstElement}, ${secondElement}`
+    );
   });
 
   // test('Should dequeue element', () => {
